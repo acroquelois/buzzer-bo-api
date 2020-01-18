2bd14769b833ae7171aee5a6c3bee7caa96802e3
Adrien Croquelois <croquelois.adrien@gmail.com>
2020-01-18 11:53:23 +0100

    add include in repo

--- buzzer-api/buzzer-api/Repository/QuestionRepository.cs
+++ buzzer-api/buzzer-api/Repository/QuestionRepository.cs
@@ -48,7 +48,10 @@ namespace buzzerApi.Repository
         {
             try
             {
-                var listQuestion = await db.Question.Include(p => p.Propositions).ToListAsync();
+                var listQuestion = await db.Question
+                    .Include(p => p.Propositions)
+                    .Include(p => p.QuestionType)
+                    .ToListAsync();
                 var question = listQuestion.First();
                 return question;
             }
